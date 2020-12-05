eba56fef3d0ce6df8358185cfa0aa05db7f9a81d
OmarYuusf <omar.atef.mohsen@gmail.com>
2020-12-04 17:28:45 -0800

    add pagination

--- index.js
+++ index.js
@@ -7,4 +7,41 @@ const isEmpty = (array) => {
   }
 };
 
-module.exports = { isEmpty };
+const paginateIt = async (page, limit, model) => {
+  var page = parseInt(page, 10) || 1;
+  var limit = parseInt(limit, 10) || 10;
+  var startIndex = (page - 1) * limit;
+
+  var endIndex = page * limit;
+  var total = await model.countDocuments();
+
+  var pagination = {};
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+  return { pagination, startIndex, limit };
+};
+
+const handler = {
+  sort: (sortQuery) => {
+    if (sortQuery) {
+      return sortQuery.select.split(",").join(" ");
+    }
+  },
+  select: (selectQuery) => {
+    if (selectQuery) {
+      return selectQuery.select.split(",").join(" ");
+    }
+  },
+};
+
+module.exports = { isEmpty, paginateIt, handler };

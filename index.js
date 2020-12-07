7508adf3e1fff0b0521912622ba4e420a2562e0a
OmarYuusf <omar.atef.mohsen@gmail.com>
2020-12-06 17:26:02 -0800

    added hash

--- index.js
+++ index.js
@@ -44,4 +44,67 @@ const handler = {
   },
 };
 
-module.exports = { isEmpty, paginateIt, handler };
+const random = {
+  num: (from, to) => {
+    return Math.round(Math.random() * (to - from) + from);
+  },
+  char: (length, isCapital) => {
+    var result = "";
+    var characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return isCapital ? result.toUpperCase() : result;
+  },
+};
+
+const hash = {
+  sign: (char, num) => {
+    let fakeNumber = "abcdefghijklmnopqrstuvwxyz";
+    let letters = "abcdefghijklmnopqrstuvwxyz" + fakeNumber.slice(0, num);
+    let lenthOfChar = char.length;
+    let arrayOfChar = [];
+    let result = num + "";
+
+    for (let i = 0; i < lenthOfChar; i++) {
+      arrayOfChar.push(char[i]);
+    }
+
+    for (let z = 0; z < arrayOfChar.length; z++) {
+      for (let j = 0; j < letters.length - num; j++) {
+        if (arrayOfChar[z] == letters[j]) {
+          result += letters[j + num];
+        }
+      }
+    }
+    return result;
+  },
+
+  design: (char) => {
+    hashNum = parseInt(char[0], 10);
+    let fakeNumber = "abcdefghijklmnopqrstuvwxyz";
+    let letters = fakeNumber.slice(-hashNum) + "abcdefghijklmnopqrstuvwxyz";
+    let lenthOfChar = char.length;
+    let arrayOfChar = [];
+    let result = "";
+    console.log(letters);
+    for (let i = 0; i < lenthOfChar; i++) {
+      arrayOfChar.push(char[i]);
+    }
+
+    for (let z = 0; z < arrayOfChar.length; z++) {
+      for (let j = 0; j < letters.length + hashNum; j++) {
+        if (arrayOfChar[z] == letters[j]) {
+          result += letters[j - hashNum];
+        }
+      }
+    }
+    return result;
+  },
+};
+
+console.log(hash.design("3ghylonlqjgrp"));
+
+module.exports = { isEmpty, paginateIt, handler, random };
